3bf9bf7480e91729d2bb3d267d7aba003568b98b
Ágatha almeida <agathasantos145@gmail.com>
2022-01-30 23:59:51 -0300

    Condição

--- Execercicios/Program.cs
+++ Execercicios/Program.cs
@@ -1,142 +1,143 @@
-﻿using DotLiquid.Util;
-using System.Globalization;
-using System.Globalization;
+﻿using dotliquid.util;
+using system.globalization;
+using system.globalization;
+using System;
 
 
-////digite um número e encontre o sucessor dele
+//////digite um número e encontre o sucessor dele
 
 int x, sucessor = 0;
 
-Console.WriteLine("Digite um número!");
+console.writeline("digite um número!");
 
-x = int.Parse(Console.ReadLine());
+x = int.parse(Console.readline());
 
 sucessor = x;
 sucessor = sucessor + 1;
-Console.WriteLine("O sucessor de  " + x + " é  " + sucessor);
-Console.ReadKey();
+console.writeline("o sucessor de  " + x + " é  " + sucessor);
+console.readkey();
 
-////Desenvolva um algoritmo que solicite a entrada de um número e calcule se o número é par ou impar.
+//////desenvolva um algoritmo que solicite a entrada de um número e calcule se o número é par ou impar.
 
-int umNumero = x;
-Console.WriteLine("Digite um número!");
-umNumero = Convert.ToInt32(Console.ReadLine()); //Lê e converte para int o número informado pelo usuário
+int umnumero = x;
+console.writeline("digite um número!");
+umnumero = convert.toint32(console.readline()); //lê e converte para int o número informado pelo usuário
 
-if (umNumero % 2 == 0)
+if (umnumero % 2 == 0)
 {
-    Console.WriteLine("Par");
+    console.writeline("par");
 }
 else
 {
-    Console.WriteLine("Impar");
+    console.writeline("impar");
 }
-Console.ReadKey();
+console.readkey();
 
 
-////Desenvolva um algoritmo que permita a entrada do nome do usuário e exiba na tela.
+//////desenvolva um algoritmo que permita a entrada do nome do usuário e exiba na tela.
 
-String Nome;
-Console.WriteLine("Escreva seu Nome!");
-Nome = Console.ReadLine();
+string nome;
+console.writeline("escreva seu nome!");
+nome = console.readline();
 
-Console.WriteLine("Meu nome é: " + Nome, "");
+console.writeline("meu nome é: " + nome, "");
 
 
 
-////Crie um algoritmo no qual o usuário/aluno deverá  informar seu nome e quatro notas.
-//// O programama deverá calcular sua média e informar se o aluno foi aprovado ou não.
-//// O número seis é o valor mínimo para aprovação.
+//////crie um algoritmo no qual o usuário/aluno deverá  informar seu nome e quatro notas.
+////// o programama deverá calcular sua média e informar se o aluno foi aprovado ou não.
+////// o número seis é o valor mínimo para aprovação.
 
-string N;
-float NotaUm = 0;
-float NotaDois = 0;
-float NotaTres = 0;
-float NotaQuatro = 0;
-Console.WriteLine("Escreva seu nome!");
-N = Console.ReadLine();
-Console.WriteLine("Escreva sua nota do primeiro bimestre");
-NotaUm = float.Parse(Console.ReadLine());
-Console.WriteLine("Escreva sua nota do segunda bimestre");
-NotaDois = float.Parse(Console.ReadLine());
-Console.WriteLine("Escreva sua nota do terceiro bimestre");
-NotaDois = float.Parse(Console.ReadLine());
-Console.WriteLine("Escreva sua nota do quarto bimestre");
-NotaQuatro = float.Parse(Console.ReadLine());
-float Soma = (NotaUm + NotaDois + NotaTres + NotaQuatro) / 4;
-if (Soma > 6)
+string n;
+float notaum = 0;
+float notadois = 0;
+float notatres = 0;
+float notaquatro = 0;
+console.writeline("escreva seu nome!");
+n = console.readline();
+console.writeline("escreva sua nota do primeiro bimestre");
+notaum = float.parse(console.readline());
+console.writeline("escreva sua nota do segunda bimestre");
+notadois = float.parse(console.readline());
+console.writeline("escreva sua nota do terceiro bimestre");
+notadois = float.parse(console.readline());
+console.writeline("escreva sua nota do quarto bimestre");
+notaquatro = float.parse(console.readline());
+float soma = (notaum + notadois + notatres + notaquatro) / 4;
+if (soma > 6)
 {
-    Console.WriteLine("O ALUNO " + N + " FOI APROVADO. \nSUA MÉDIA FINAL FOI " + Soma);
+    console.writeline("o aluno " + n + " foi aprovado. \nsua média final foi " + soma);
 }
 else
 {
-    Console.WriteLine("O ALUNO " + N + " FOI Reprovado. \nSUA MÉDIA FINAL FOI " + Soma);
+    console.writeline("o aluno " + n + " foi reprovado. \nsua média final foi " + soma);
 }
-Console.ReadLine();
+console.readline();
 
-////Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
-////mensagem explicativa, conforme exemplos
+//////faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+//////mensagem explicativa, conforme exemplos
 
 
 
-Console.WriteLine("Escreva um valor:");
+console.writeline("escreva um valor:");
 int valor = 0;
-valor = int.Parse(Console.ReadLine());
-Console.WriteLine("Escreva o segundo valor:");
-int valorUm = 0;
-valorUm = int.Parse(Console.ReadLine());
-float soma = (valor + valorUm);
-Console.WriteLine("A soma é " + soma);
+valor = int.parse(console.readline());
+console.writeline("escreva o segundo valor:");
+int valorum = 0;
+valorum = int.parse(console.readline());
+float soma = (valor + valorum);
+console.writeline("a soma é " + soma);
 
-////Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
-////casas decimais conforme exemplos.Fórmula da área: area = π.raio2
-////Considere o valor de π = 3.14159
+//////faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//////casas decimais conforme exemplos.fórmula da área: area = π.raio2
+//////considere o valor de π = 3.14159
 
 double r, a, pi = 3.14159;
 
-r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+r = double.parse(console.readline(), cultureinfo.invariantculture);
 
 a = pi * r * r;
 
-Console.WriteLine("A=" + a.ToString("F4", CultureInfo.InvariantCulture));
+console.writeline("a=" + a.tostring("f4", cultureinfo.invariantculture));
 
-////Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
-////de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+//////fazer um programa para ler quatro valores inteiros a, b, c e d. a seguir, calcule e mostre a diferença do produto
+//////de a e b pelo produto de c e d segundo a fórmula: diferenca = (a * b - c * d).
 
-int A = 0;
-Console.WriteLine("Digite um valor inteiro:");
-A = int.Parse(Console.ReadLine());
-int B = 0;
-Console.WriteLine("Digite um valor inteiro:");
-B = int.Parse(Console.ReadLine());
-int C = 0;
-Console.WriteLine("Digite um valor inteiro:");
-C = int.Parse(Console.ReadLine());
-int D = 0;
-Console.WriteLine("Digite um valor inteiro:");
-D = int.Parse(Console.ReadLine());
+int a = 0;
+console.writeline("digite um valor inteiro:");
+a = int.parse(console.readline());
+int b = 0;
+console.writeline("digite um valor inteiro:");
+b = int.parse(console.readline());
+int c = 0;
+console.writeline("digite um valor inteiro:");
+c = int.parse(console.readline());
+int d = 0;
+console.writeline("digite um valor inteiro:");
+d = int.parse(console.readline());
 double calculo;
-calculo = (A * B - C * D);
-Console.WriteLine("A diferença do produto é " + calculo);
+calculo = (a * b - c * d);
+console.writeline("a diferença do produto é " + calculo);
 
 
-//Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
-//hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
-//decimais
+////fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+////hora e calcula o salário desse funcionário. a seguir, mostre o número e o salário do funcionário, com duas casas
+////decimais
 
 int numbr = 0;
-Console.WriteLine("Digite seu número de funcionário:");
-numbr = int.Parse(Console.ReadLine());
+console.writeline("digite seu número de funcionário:");
+numbr = int.parse(console.readline());
 int horas = 0;
-Console.WriteLine("Digite suas horas trabalhadas:");
-horas = int.Parse(Console.ReadLine());
+console.writeline("digite suas horas trabalhadas:");
+horas = int.parse(console.readline());
 int valor = 0;
-Console.WriteLine("Digit quanto você ganha por hora:");
-valor = int.Parse(Console.ReadLine());
+console.writeline("digit quanto você ganha por hora:");
+valor = int.parse(console.readline());
 int calculo = (horas * valor);
-Console.WriteLine("Seu salário é de:" + calculo);
+////console.writeline("seu salário é de:" + calculo);
 
-//Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
-//código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+////fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+////código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. calcule e mostre o valor a ser pago.
 
 
 
@@ -144,16 +145,30 @@ Console.WriteLine("Seu salário é de:" + calculo);
 int cod1, cod2, qte1, qte2;
 double preco1, preco2, total;
 
-string[] valores = Console.ReadLine().Split(' ');
-cod1 = int.Parse(valores[0]);
-qte1 = int.Parse(valores[1]);
-preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+string[] valores = console.readline().split(' ');
+cod1 = int.parse(valores[0]);
+qte1 = int.parse(valores[1]);
+preco1 = double.parse(valores[2], cultureinfo.invariantculture);
 
-valores = Console.ReadLine().Split(' ');
-cod2 = int.Parse(valores[0]);
-qte2 = int.Parse(valores[1]);
-preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+valores = console.readline().split(' ');
+cod2 = int.parse(valores[0]);
+qte2 = int.parse(valores[1]);
+preco2 = double.parse(valores[2], cultureinfo.invariantculture);
 
 total = preco1 * qte1 + preco2 * qte2;
 
-Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+console.writeline("valor a pagar: r$ " + total.tostring("f2", cultureinfo.invariantculture));
+
+////fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+int x;
+console.writeline("digite um número:");
+x = int.parse(console.readline());
+if (x < 0)
+{
+    console.writeline("negativo");
+}
+else
+{
+    console.writeline("não negativo");
+}
