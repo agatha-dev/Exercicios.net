74803402f4fa49d4cd4fe604c3ae633767609007
Ágatha almeida <agathasantos145@gmail.com>
2022-04-17 22:30:10 -0300

    Valor de PI

--- Execercicios/Program.cs
+++ Execercicios/Program.cs
@@ -341,3 +341,16 @@ else
     Console.WriteLine("Sem permissão");
 }
 
+//Calculadora calc = new Calculadora();
+
+//Entre com o valor do pi
+
+Console.Write("Entre com o valor do raio:");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double circ = Calculadora.Circunferencia(raio);
+double volume = Calculadora.Volume(raio);
+
+Console.WriteLine("Circunferencia:" + circ.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
